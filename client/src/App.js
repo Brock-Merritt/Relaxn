7fd8cef6c94eb0d469c3b0c3b770e2d1e165cf81
Tari <bota.maral@gmail.com>
2022-01-26 12:44:53 -0500

    create login, connect Apollo server, install dependencies for all

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,54 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
 
-import Header from '.components/Header'
+import Header from "./components/Header";
 
-import Login from './Pages/Login';
-import Signup from './Pages/Signup';
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <Router>
-    <div className="App">
-      <Header/>
-      <div className="container">
-    <Switch>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="flex-column justify-flex-start min-100-vh">
+          <Header />
+          <div className="container">
+            <Switch>
               <Route exact path="/login" component={Login} />
               <Route exact path="/signup" component={Signup} />
-    </Switch>
-      </div>
-    </div>
-    </Router>
+
+            </Switch>
+          </div>
+          
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
