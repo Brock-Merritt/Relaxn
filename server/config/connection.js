9d603cecba1c8a7be50d66444ff5aed6c8d3c8a6
Your name here <brkmer@gmail.com>
2022-01-26 12:49:11 -0500

    added to gitigore, added connection.js, models/index

---
+++ server/config/connection.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+
+mongoose.connect(proces.env.MONGODB_URI || 'mongodb://localhost/relaxndb', 
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+      }
+    );
+
+
+    
+    
+module.exports = mongoose.connection;
+    
+
