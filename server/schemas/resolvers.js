7771cebb8666cb3fda849475e90f995c42beafab
Tari <bota.maral@gmail.com>
2022-01-26 12:10:35 -0500

    update server.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -52,3 +52,4 @@ const resolvers = {
         }
     }
 };
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 
+const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
