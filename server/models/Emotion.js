f84a4da4a6f748733d61e9f4f9d3ed2f7f9ee5af
Your name here <brkmer@gmail.com>
2022-01-25 20:48:58 -0500

    moved emotions into pages

---
+++ server/models/Emotion.js
@@ -0,0 +1,13 @@
+const { Schema } = require('mongoose');
+
+const emotionSchema = new Schema({
+    placeholder: [
+        {
+            type: String,
+        }
+    ],
+
+    emotion: {
+        
+    }
+})
