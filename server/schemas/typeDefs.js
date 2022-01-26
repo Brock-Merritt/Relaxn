e91acf95b6da491a1ae3131b10a765b431527a65
Tari <bota.maral@gmail.com>
2022-01-26 12:08:04 -0500

    create resolvers and typeDefs for server

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,26 @@
+// import the gql tagged template function
+const { gql } = require("apollo-server-express");
+
+// create our typeDefs
+const typeDefs = gql`
+type User {
+    _id: ID
+  username: String
+  email: String
+}
+type Auth {
+    token: ID!
+    user: User
+  }
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+`;
+// export the typeDefs
+module.exports = typeDefs;
