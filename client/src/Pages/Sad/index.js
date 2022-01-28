695d9b5f82e76ed0c90e978c5cda1417e3564d7e
caseylister <caseynlister@gmail.com>
2022-01-28 15:09:20 -0500

    playing with Sad cards

---
+++ client/src/Pages/Sad/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+function Sad(){
+    return(
+        <>
+        <div className="card">
+            <p>card 1</p>
+            <div>Insert card image here</div>
+            <div className="card-body">testing1</div>
+            <div className="card-text">testing2</div>
+        </div>
+        <div className="card">
+            <p>card 1</p>
+            <div>Insert card image here</div>
+            <div className="card-body">testing1</div>
+            <div className="card-text">testing2</div>
+        </div>
+        <div className="card">
+            <p>card 1</p>
+            <div>Insert card image here</div>
+            <div className="card-body">testing1</div>
+            <div className="card-text">testing2</div>
+        </div>
+        <div className="card">
+            <p>card 1</p>
+            <div>Insert card image here</div>
+            <div className="card-body">testing1</div>
+            <div className="card-text">testing2</div>
+        </div>
+        </>
+    )
+}
