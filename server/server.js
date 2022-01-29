35b7cd7d0a44ce592f43d01858440c40a0cdf2eb
detrasmental <detra.gordon76@gmail.com>
2022-01-29 10:51:06 -0500

    server updates

--- server/server.js
+++ server/server.js
@@ -8,16 +8,20 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
+
+const startServer = async () => {
+const server = new ApolloServer({ 
+    typeDefs, 
+    resolvers, 
+    context: authMiddleware 
 });
 
 await server.start();
 server.applyMiddleware({ app });
+};
 
-app.use(express.urlencoded({ extended: false }));
+startServer()
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
@@ -31,6 +35,6 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
