15ea533aa675b4bd3ba9fc29014c9aeb927fa83e
Your name here <brkmer@gmail.com>
2022-01-29 10:24:57 -0500

    a[o0sdfihkbakrwej\k

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+const startServer = async () => {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -16,7 +18,9 @@ const server = new ApolloServer({
 
 await server.start();
 server.applyMiddleware({ app });
+};
 
+startServer()
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
@@ -31,6 +35,6 @@ app.get('*', (req, res) => {
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    // console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
 });
