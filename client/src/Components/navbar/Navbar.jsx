21e94c41af7c92e55edc669078564f404c990c18
Tari <bota.maral@gmail.com>
2022-02-03 15:22:23 -0500

    fixed hide login/signup option after login

--- client/src/Components/navbar/Navbar.jsx
+++ client/src/Components/navbar/Navbar.jsx
@@ -1,42 +1,49 @@
-import React from 'react';
-import './Navbar.css';
-import AuthService from "../../utils/auth";
+import React from "react";
+import "./Navbar.css";
+import Auth from "../../utils/auth";
 
-function Navbar () {
-    const logout = (event) => {
-            event.preventDefault();
-            AuthService.logout();
-    };
+function Navbar() {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+  return (
+    <section className="navbar">
+      <a href="/" className="navbar-item">
+        Home
+      </a>
+      <a href="/About.html" className="navbar-item">
+        About
+      </a>
 
+      {Auth.loggedIn() ? (
+        <>
+          <a href="/login" onClick={logout} className="navbar-item">
+            Logout
+          </a>
+        </>
+      ) : (
+        <>
+          <a href="/login" className="navbar-item">
+            Login
+          </a>
+          <a href="/signup" className="navbar-item">
+            Signup
+          </a>
+        </>
+      )}
 
-
-    return (
-        <section className="navbar">
-        <a href="/" className="navbar-item">Home</a>
-        <a href="/About.html" className="navbar-item">About</a>
-        <a href="/login" className="navbar-item">Login</a>
-        <a href="/signup" className="navbar-item">Signup</a>
-        <a href="https://buy.stripe.com/test_3cseYn4CM5bi4Uw5kl" target="_blank" rel="noreferrer" className="navbar-item" >Donate</a>
-        <nav className='navbar'>
-          {AuthService.loggedIn() ? (
-            <>
-              <a href="/profile" className="navbar-item">View Profile</a>
-              <a href="/" className="navbar-item" onClick={logout}>
-                Logout
-              </a>
-            </>
-          ) : (
-            <>
-              {/* <Link to="/login">Login</Link>
-              <Link to="/signup">Signup</Link>
-              <Link to="/profile">Profile</Link>
-              <a href="https://buy.stripe.com/test_3cseYn4CM5bi4Uw5kl" target="_blank" rel="noreferrer">Donate</a> */}
-
-            </>
-          )}
-        </nav>
+      <a
+        href="https://buy.stripe.com/test_3cseYn4CM5bi4Uw5kl"
+        target="_blank"
+        rel="noreferrer"
+        className="navbar-item"
+      >
+        Donate
+      </a>
+      <nav className="navbar"></nav>
     </section>
-    )
+  );
 }
 
 export default Navbar;
